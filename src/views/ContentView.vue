2c60ec3cca0cc2748db4d02f401be49cf7677c8f
Sean Li <lixiaoheng@gmail.com>
2025-08-07 11:37:57 +0800

    fix: Move divider to the top of the content view

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -15,10 +15,10 @@ watch(config, (n) => (transform.value = `rotate(${n.turn}turn)`))
 <template>
   <main id="container">
     <section id="content">
+      <hr />
       <section id="w1">
         <DateTime :date="moment" />
       </section>
-      <hr />
       <section id="w2">
         <EverydayNews />
       </section>
@@ -41,9 +41,6 @@ watch(config, (n) => (transform.value = `rotate(${n.turn}turn)`))
 
   overflow: hidden;
   display: grid;
-  grid-template-areas:
-    'w1'
-    'w2';
   grid-template-columns: 1fr;
   grid-template-rows: auto auto 1fr;
   gap: 1rem;
@@ -63,7 +60,6 @@ hr {
 
 #w1 {
   display: flex;
-  grid-area: w1;
   align-items: center;
   justify-content: center;
 
@@ -73,7 +69,6 @@ hr {
 #w2 {
   overflow: hidden;
   display: flex;
-  grid-area: w2;
   align-items: stretch;
   justify-content: center;
 
