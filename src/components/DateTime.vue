3e1696b5043ee87d9ae80e80bfc677abddbaaa3f
Sean Li <lixiaoheng@gmail.com>
2025-08-07 11:29:30 +0800

    fix: Correct template structure for DateTime component

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -10,24 +10,26 @@ const { name, rest } = useNextHolidayApi(props.date)
 </script>
 
 <template>
-  <section id="datetime">
-    <p id="moment">{{ getTime(props.date) }}</p>
-    <p>
-      <span>
-        今天是<span class="hightlight">{{ getYear(props.date) }}</span
-        >年<span class="hightlight">{{ getMonth(props.date) }}</span
-        >月<span class="hightlight">{{ getDay(props.date) }}</span
-        >日<span class="hightlight">{{ getWeekday(props.date) }}</span>
-      </span>
-      <span v-if="isDayOff"> 今天是<span class="hightlight">休息日</span>，放松一下吧</span>
-      <span v-else>
-        距离下一个休息日<span class="hightlight">{{ name }}</span
-        >还有<span class="hightlight">{{ rest }}</span
-        >天
-      </span>
-    </p>
-  </section>
-  <hr />
+  <div>
+    <section id="datetime">
+      <p id="moment">{{ getTime(props.date) }}</p>
+      <p>
+        <span>
+          今天是<span class="hightlight">{{ getYear(props.date) }}</span
+          >年<span class="hightlight">{{ getMonth(props.date) }}</span
+          >月<span class="hightlight">{{ getDay(props.date) }}</span
+          >日<span class="hightlight">{{ getWeekday(props.date) }}</span>
+        </span>
+        <span v-if="isDayOff"> 今天是<span class="hightlight">休息日</span>，放松一下吧</span>
+        <span v-else>
+          距离下一个休息日<span class="hightlight">{{ name }}</span
+          >还有<span class="hightlight">{{ rest }}</span
+          >天
+        </span>
+      </p>
+    </section>
+    <hr />
+  </div>
 </template>
 
 <style scoped>
