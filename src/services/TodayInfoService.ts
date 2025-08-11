3621f72f9689c03d7d9dd738d0d9a3116a542e28
Sean Li <lixiaoheng@gmail.com>
2025-08-11 17:20:30 +0800

    refactor: remove obsolete files and directories

--- src/services/TodayInfoService.ts
+++
@@ -1,7 +0,0 @@
-import { ApiService } from './ApiService'
-
-export class TodayInfoService extends ApiService {
-  constructor(date: string) {
-    super(`https://timor.tech/api/holiday/info/${date}`)
-  }
-}
